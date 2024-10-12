68846ab2f0893b51adb4f27d0accbe7f01bd9ad0
evencio <dev.evenciohernandez@gmail.com>
2024-10-12 18:27:02 -0400

    Solution

--- index.js
+++ index.js
@@ -1,53 +1,109 @@
-// let savedCount = 0
 /**
- * @param {CallableFunction} connect 
- * @param {CallableFunction} save 
+ * @typedef {Object} IConnection
+ * @property {Function} download
+ * @property {Function} close
+*/
+
+/**
+ * @param {Function} connect
+ * @param {number} maxConcurrency
+ * @returns {IConnection[]}
+*/
+const createConnections = async (connect, maxConcurrency) => {
+  const connections = [];
+  try {
+    // Create as many connections as posible
+    for (let i = 0; i < maxConcurrency; i++) {
+      connections.push(await connect());
+    }
+  } catch (error) {
+    // Ignore if a connection fail
+  }
+  // Check if at less one connection was successful
+  if (!connections.length)
+    throw new Error("connection failed");
+  
+  return connections;
+}
+
+/**
+ * @param {IConnection[]} connections
+*/
+const closeConnections = (connections) => {
+  for (const connection of connections) {
+    connection.close();
+  }
+  return true;
+}
+
+/**
+ * @param {IConnection[]} connections 
+ * @param {Function} save
  * @param {string} url
 */
-const createPromise = (connect, save, url) => {
-  return new Promise(async (resolve, reject) => {
-    let connection, error;
-    try {
-      connection = await connect();
-
-      await save(await connection.download(url));
-      // savedCount++;
-      connection.close();
-      resolve(true);
-    } catch (error) {
-      if (connection)
-        connection.close();
-
-      resolve(error);
-    }
-  })
+const createPromises = (connections, save, url) => {
+  /** @type {Promise<boolean>[]} */
+  const promises = []
+  for (const connection of connections) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        // Download file
+        resolve(connection.download(url));
+      }).then(
+        (value) => save(value)
+      ).then((value) => {
+        return true;
+      })
+    );
+  }
+  return promises
 }
 
 /**
- * 
- * @param {CallableFunction} connect 
- * @param {CallableFunction} save 
+ * @param {Function} connect
+ * @param {Function} save 
  * @param {string[]} downloadList 
- * @param {number} maxConcurrency 
- * @returns 
+ * @param {number} maxConcurrency
 */
 const pooledDownload = async (connect, save, downloadList, maxConcurrency) => {
-  if (!(downloadList.length && maxConcurrency)) return [];
-  /** @type {Promise[]} */
-  let promises = [];
-  let results;
+  if (!(downloadList.length && maxConcurrency)) return;
+  // Create connections
+  /** @type {IConnection[]} */
+  const connections = await createConnections(connect, maxConcurrency);
+  /** @type {string[]} */
+  let urls = [],
+    savedCount = 0,
+    /** @type {Error} */ error;
+  // Iterate url list
   for (const e of downloadList) {
-    promises.push(createPromise(connect, save, e));
-    if (promises.length < maxConcurrency) continue;
-    // console.log(promises.length);
+    // Ignore invalid urls
+    if (!e.length) continue;
+    // Start adding promises to the queue
+    urls.push(e);
+    // Check if have the same numbers of urls that active connections
+    if (urls.length < connections.length)
+      continue;
     try {
-      results = await Promise.all(promises);
-    } catch (error) {
-      throw new Error("connection failed");
+      // Executed downloads batch
+      savedCount += (await Promise.all(createPromises(connections, save, e))).length;
+    } catch (err) {
+      // Save error if something fails and break loop
+      error = err;
+      break;
     }
-    promises = [];
+    // Reset promises queue
+    urls = [];
   }
-  // console.log(savedCount);
+  // Close connections
+  await closeConnections(connections);
+  // Check if error downloading
+  if (error)
+    // throw new Error("unexpected error during download");
+    throw new Error("connection failed");
+
+  console.log("Total saved files:", savedCount);
+  return true;
 }
 
-module.exports = pooledDownload
+
+module.exports = pooledDownload;
